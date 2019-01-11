524ab221efae51aa5cbd74accad707035350d8f8
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2019-01-11 14:00:39 +0530

    Create arms.py

---
+++ arms.py
@@ -0,0 +1,13 @@
+x = int(input("Enter a number: "))  
+s = 0  
+t = x  
+  
+while t > 0:  
+   digit = t % 10  
+   s += digit ** 3  
+   t //= 10  
+  
+if x == s:  
+   print("yes")  
+else:  
+   print("no")  
