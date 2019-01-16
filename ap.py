eeebd748a52a7126569534ba92136c93fd5baba8
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2019-01-16 20:26:10 +0530

    Update ap.py

--- ap.py
+++ ap.py
@@ -1,4 +1,4 @@
-def sumofAP(n2,a2,d2):
+def sumofAP1(n2,a2,d2):
     su=0
     i=0
     while i<n2:
@@ -7,4 +7,4 @@ def sumofAP(n2,a2,d2):
         i+=1
     return su    
 n2,a2,d2=map(int,input().split())
-print(sumofAP(n2,a2,d2))
+print(sumofAP1(n2,a2,d2))
