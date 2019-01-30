8d0b6397ac73d356dba4a73533311518d8d19268
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2019-01-30 16:18:36 +0530

    Create samepoint.py

---
+++ samepoint.py
@@ -0,0 +1,9 @@
+a1,a2=map(int,input().split())
+b1,b2=map(int,input().split())
+c1,c2=map(int,input().split())
+if a1==b1==c1 or a2==b2==c2:
+    print("yes")
+elif a1==a2 or b1==b2 or c1==c2:
+    print("yes")
+else:
+    print("no")
