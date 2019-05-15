f0c0e8f73f68c0c150a20767deaaf78341922ad7
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2019-05-15 11:38:34 +0530

    Create pro17.py

---
+++ pro17.py
@@ -0,0 +1,11 @@
+n,k=map(int,input().split())
+li=list(map(int,input().split()))
+for i in range(0,n-1):
+  for j in range(i+1,n):
+    if li[i]+li[j]==k:
+      c=1
+  break
+if c==1:
+  print("yes")
+else:
+  print("no")    
