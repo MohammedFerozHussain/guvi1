cf6c6320e5e48a903a589b4cc34b3cea2923d6ae
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2019-05-18 11:07:22 +0530

    Update hunter3.py

--- hunter3.py
+++ hunter3.py
@@ -1,8 +1,12 @@
 n=int(input())
 li=list(map(int,input().split()))
 r=[]
+c1=[]
 for i,j in enumerate(li):
     if i==j:
         r.append(j)
-        c=sorted(r)
-print(*c)        
+c1=sorted(r)
+if c1:
+    print(*c1)
+else:
+    print(-1)
