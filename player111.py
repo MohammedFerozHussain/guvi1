1b70e88ba50d41dd95dd483b72032697afadc9b3
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2019-06-08 16:49:16 +0530

    Create player111.py

---
+++ player111.py
@@ -0,0 +1,14 @@
+n,m=map(int,input().split())
+li=list(map(int,input().split()))
+r=[]
+for i in range(0,n-1):
+    count=1
+    for j in li[i+1:]:
+        if li[i]==j:
+            count+=1
+            break
+    if count>1 and j not in r:
+        r.append(j)
+if r:
+    c=sorted(r)
+    print(*c)
